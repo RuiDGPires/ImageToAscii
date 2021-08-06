2ce793850ed015662621027ebc6453ddfd21412f
RuiDGPires <pires.rui@outlook.pt>
2021-08-06 20:16:32 +0100

    More or less working

--- src/main.c
+++ src/main.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define OUTPUT_SCALE 0.1
+#define CONTRAST_MUL 900000
+#define NCHARS 15
+const char table[NCHARS] = {'.', '-', ':', ';', ')', '+', 'x', '7', 'O', '8', '%', '@', 'N', 'M','#'};
+
+char ColorToChar(int val){
+    val = 255 - val;
+    return table[(NCHARS - 1) * val / 255];
+}
+
 void throwError(char message[]){
     fprintf(stderr, "\033[1;31m");
     fprintf(stderr,"%s\n", message);
@@ -9,20 +19,49 @@ void throwError(char message[]){
     exit(0);
 }
 
-void doStuff(FIBITMAP *bm){
-    if (!FreeImage_AdjustContrast(bm, 400)) throwError("Adjust Contrast Failed");
+char *doStuff(FIBITMAP *bm){
+    int out_width = FreeImage_GetWidth(bm)*OUTPUT_SCALE, out_height = FreeImage_GetHeight(bm)*OUTPUT_SCALE;
+    FreeImage_SetTransparent(bm, FALSE);
+    *bm = *FreeImage_ConvertTo24Bits(bm);
+    
+    if (!FreeImage_AdjustContrast(bm, CONTRAST_MUL)) throwError("Adjust Contrast Failed");
     *bm = *FreeImage_ConvertToGreyscale(bm);
+    *bm = *FreeImage_Rescale(bm, out_width, out_height, FILTER_BOX);
+
+    char *buffer;
+    unsigned buffer_size = sizeof(char)*(out_width + 1)*out_height;
+    unsigned write_index = buffer_size - 1;
+
+    if ((buffer = malloc(buffer_size)) == NULL) throwError("Error allocating memory");
+
+    // Calculate the number of bytes per pixel (3 for 24-bit or 4 for 32-bit)
+    int bytespp = FreeImage_GetLine(bm) / out_width;
+
+
+    for (unsigned y = 0; y < out_height; ++y){
+        BYTE *bits = FreeImage_GetScanLine(bm, y);
+        for (unsigned x = 0; x < out_width; ++x){
+            buffer[write_index--] = ColorToChar(bits[FI_RGBA_RED]);
+            bits += bytespp;
+        }
+        buffer[write_index--] = '\n';
+    }
+
+    return buffer;
 }
 
 int main(int argc, char *argv[]){
     FreeImage_Initialise(TRUE);
     printf("Image to Ascii\nMade using FreeImage %s\n", FreeImage_GetVersion());
 
-    FIBITMAP *bitmap = FreeImage_Load(FIF_PNG, "sample.png", BMP_DEFAULT);
+    FIBITMAP *bitmap = FreeImage_Load(FIF_PNG, "github.png", BMP_DEFAULT);
     if (bitmap) {
-        doStuff(bitmap);
+        char *buffer = doStuff(bitmap);
         if (!FreeImage_Save(FIF_PNG, bitmap, "sample-output.png", PNG_DEFAULT)) throwError("Error Saving Image");
         FreeImage_Unload(bitmap);
+
+        printf("%s\n", buffer);
+        free(buffer);
     }else throwError("Error Opening File");
 
     

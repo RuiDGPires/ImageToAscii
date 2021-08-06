da4fa32f92d0f0abd7fc9ae40af0114c4ad728b8
RuiDGPires <pires.rui@outlook.pt>
2021-08-06 18:45:00 +0100

    added grayscaling

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <FreeImage.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void throwError(char message[]){
+    fprintf(stderr, "\033[1;31m");
+    fprintf(stderr,"%s\n", message);
+    fprintf(stderr, "\033[0m");
+    exit(0);
+}
 
 void doStuff(FIBITMAP *bm){
-    if (FreeImage_AdjustContrast(bm, 400)){
-        printf("yey!\n");
-    }
+    if (!FreeImage_AdjustContrast(bm, 400)) throwError("Adjust Contrast Failed");
+    *bm = *FreeImage_ConvertToGreyscale(bm);
 }
 
 int main(int argc, char *argv[]){
@@ -14,11 +21,9 @@ int main(int argc, char *argv[]){
     FIBITMAP *bitmap = FreeImage_Load(FIF_PNG, "sample.png", BMP_DEFAULT);
     if (bitmap) {
         doStuff(bitmap);
-        FreeImage_Save(FIF_PNG, bitmap, "sample-output.png", PNG_DEFAULT);
+        if (!FreeImage_Save(FIF_PNG, bitmap, "sample-output.png", PNG_DEFAULT)) throwError("Error Saving Image");
         FreeImage_Unload(bitmap);
-    }else {
-        printf("Error opening file\n");
-    }
+    }else throwError("Error Opening File");
 
     
 
